10b12e1abbf047178e366868b59532551bf05eaf
Daria1508 <130033981+Daria1508@users.noreply.github.com>
2023-04-07 20:20:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1307537098 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    a=(2(x.mean()))/484
+    return a # Ваш ответ
