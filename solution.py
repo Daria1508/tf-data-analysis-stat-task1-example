41056b6d8e7cf2238be7ed71d65e2319706fb876
Daria1508 <130033981+Daria1508@users.noreply.github.com>
2023-04-07 21:45:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import numpy as np
 chat_id = 1307537098 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a=(2*(x.mean()))/484
+    a=(2*x.mean())/484
     return a # Ваш ответ
